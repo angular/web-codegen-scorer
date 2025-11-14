59123826aa5264611e509b43f685ec733730837a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-11-14 12:03:37 +0000

    feat: support annotating/labeling ratings for future grouping
    * Supports adding grouping labels to ratings. The labels can be used to
      identify similar-related ratings— but nothing in the UI or UI JSON is
      doing the grouping yet. The labels are exposed to the individual
      checks though.

--- runner/ratings/built-in-ratings/user-journeys-rating.ts
+++ runner/ratings/built-in-ratings/user-journeys-rating.ts
@@ -7,6 +7,7 @@ export const userJourneysRating: PerBuildRating = {
   description: 'Ensures that all User Journeys are working in the generated app',
   kind: RatingKind.PER_BUILD,
   category: RatingCategory.MEDIUM_IMPACT,
+  groupingLabels: ['functionality', 'runability', 'interaction-testing'],
   scoreReduction: '30%',
   rate: ({serveResult}) => {
     if (serveResult === null || serveResult.userJourneyAgentOutput === null) {
