ec32344ce994c84103c9d9377d3d0afabf203a1f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2026-01-12 17:57:57 +0000

    refactor: improve rubrics extraction
    * Instead of computing a weighted global average, build weighted averages per app and then average out for the full run.
    * Take results directly to allow for more control.

--- runner/utils/extract-rubrics.ts
+++ runner/utils/extract-rubrics.ts
@@ -1,13 +1,15 @@
-import {IndividualAssessmentState, RunInfo} from '../shared-interfaces.js';
+import {AssessmentResult, IndividualAssessmentState} from '../shared-interfaces.js';
 
 export interface RubricInfo {
   score: number;
 }
 
-export function extractRubrics(run: RunInfo): Record<string, RubricInfo> {
-  const rubricsAnalysis: Record<string, {scores: {value: number; weight: number}[]}> = {};
+export function extractRubrics(results: AssessmentResult[]): Record<string, RubricInfo> {
+  const rubricsBreakdown: Record<string, number[]> = {};
 
-  for (const app of run.results) {
+  for (const app of results) {
+    const rubricsAnalysis: Record<string, {scores: {value: number; weight: number}[]}> = {};
+
     for (const category of app.score.categories) {
       for (const check of category.assessments) {
         if (check.state === IndividualAssessmentState.SKIPPED) {
@@ -29,16 +31,22 @@ export function extractRubrics(run: RunInfo): Record<string, RubricInfo> {
         }
       }
     }
+
+    for (const label in rubricsAnalysis) {
+      const scores = rubricsAnalysis[label]!.scores;
+      const numerator = scores.reduce((sum, score) => sum + score.value, 0);
+      const denominator = scores.reduce((sum, score) => sum + score.weight, 0);
+
+      rubricsBreakdown[label] ??= [];
+      rubricsBreakdown[label].push(numerator / denominator);
+    }
   }
 
-  const rubricsBreakdown: Record<string, RubricInfo> = {};
-  for (const label in rubricsAnalysis) {
-    const scores = rubricsAnalysis[label]!.scores;
-    const numerator = scores.reduce((sum, score) => sum + score.value, 0);
-    const denominator = scores.reduce((sum, score) => sum + score.weight, 0);
-    rubricsBreakdown[label] = {
-      score: numerator / denominator,
-    };
-  }
-  return rubricsBreakdown;
+  return Object.entries(rubricsBreakdown).reduce(
+    (rubricsResult, [label, scores]) => ({
+      ...rubricsResult,
+      [label]: scores.reduce((prev, cur) => prev + cur, 0) / scores.length,
+    }),
+    {},
+  );
 }
